f88f0ad486785aba725dfade0971065327270205
max-programming <ui.sabuwala@gmail.com>
2020-04-25 13:05:30 +0530

    Added favicon and a link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { useState } from "react";
 import AddTodo from "./components/AddTodo";
 import Todo from "./components/Todo";
+import { Link } from "@material-ui/core";
 
 function App() {
   const [todos, setTodos] = useState([]);
-
+  const channelLink = {
+    float: "right",
+    fontWeight: "bold",
+    fontSize: 25,
+  };
   return (
     <div>
+      <Link
+        href="https://youtube.com/c/MaxProgramming"
+        target="_blank"
+        color="error"
+        style={channelLink}
+      >
+        Max Programming
+      </Link>
       <AddTodo makeTodos={(text) => setTodos([...todos, text])} />
       {todos.map((todo, index) => {
         return <Todo todoNo={index} todo={todo} key={index} />;
