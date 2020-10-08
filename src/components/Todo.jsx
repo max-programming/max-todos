0ee66ba0801ae62dacdb6ad68e2feee272571051
Nicholas <11728676+ychan052@users.noreply.github.com>
2020-10-08 15:21:32 -0700

    Added simple edit todo functionality

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,36 +1,43 @@
-import React from "react";
+import React from 'react';
 import {
   Card,
   CardContent,
   Typography,
   Container,
   IconButton,
-} from "@material-ui/core";
-import { Check, Delete } from "@material-ui/icons";
+} from '@material-ui/core';
+import { Check, Delete } from '@material-ui/icons';
+import EditIcon from '@material-ui/icons/Edit';
 
-const Todo = ({ todo, markComplete, delTodo }) => {
-  let checkedStyle = { textDecoration: "none" };
-  if (todo.completed) checkedStyle.textDecoration = "line-through";
-  else checkedStyle.textDecoration = "none";
+const Todo = ({ todo, markComplete, delTodo, editTodo }) => {
+  let checkedStyle = { textDecoration: 'none' };
+  if (todo.completed) checkedStyle.textDecoration = 'line-through';
+  else checkedStyle.textDecoration = 'none';
   // todo.completed ? (checkedStyle.textDecoration = "line-through") : null;
   return (
     <Container>
       <Card
         className="root"
         variant="outlined"
-        style={{ marginTop: 35, background: "lightgray" }}
+        style={{ marginTop: 35, background: 'lightgray' }}
       >
         <CardContent>
           <Typography variant="h5" component="h2" style={checkedStyle}>
             <IconButton onClick={markComplete.bind(this, todo.id)}>
-              <Check style={{ color: "green" }} />
+              <Check style={{ color: 'green' }} />
             </IconButton>
-            { todo.title }
+            {todo.title}
             <IconButton
-              style={{ float: "right" }}
+              style={{ float: 'right' }}
               onClick={delTodo.bind(this, todo.id)}
             >
-              <Delete style={{ color: "red" }} />
+              <Delete style={{ color: 'red' }} />
+            </IconButton>
+            <IconButton
+              style={{ float: 'right' }}
+              onClick={editTodo.bind(this, todo.id)}
+            >
+              <EditIcon style={{ color: 'yellow' }} />
             </IconButton>
           </Typography>
         </CardContent>
