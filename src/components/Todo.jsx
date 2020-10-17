81f3957d1c06720d77fbeda1b8926076baa42893
Caleb Miller <caleb@mathmammoth.com>
2020-10-17 14:27:01 -0600

    use pretty material ui dialogs

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,64 +1,58 @@
-import React from "react";
-import {
-  Card,
-  CardContent,
-  Typography,
-  Container,
-  IconButton,
-  useMediaQuery,
-} from "@material-ui/core";
-import CheckTwoToneIcon from "@material-ui/icons/CheckTwoTone";
-import DeleteTwoToneIcon from "@material-ui/icons/DeleteTwoTone";
-import EditTwoToneIcon from "@material-ui/icons/EditTwoTone";
+import React, { useState } from 'react';
+import { Card, CardContent, Typography, Container, IconButton, useMediaQuery } from '@material-ui/core';
+import CheckTwoToneIcon from '@material-ui/icons/CheckTwoTone';
+import DeleteTwoToneIcon from '@material-ui/icons/DeleteTwoTone';
+import EditTwoToneIcon from '@material-ui/icons/EditTwoTone';
+import DeleteConfirm from './DeleteConfirm';
+import EditConfirm from './EditConfirm';
 
 const Todo = ({ todo, markComplete, delTodo, editTodo }) => {
-  const matches = useMediaQuery("(max-width: 768px)");
-  let checkedStyle = { textDecoration: "none" };
-  if (todo.completed) checkedStyle.textDecoration = "line-through";
-  else checkedStyle.textDecoration = "none";
+  const matches = useMediaQuery('(max-width: 768px)');
+  const [deleteOpen, setDeleteOpen] = useState(false);
+  const [editOpen, setEditOpen] = useState(false);
+  let checkedStyle = { textDecoration: 'none' };
+  if (todo.completed) checkedStyle.textDecoration = 'line-through';
+  else checkedStyle.textDecoration = 'none';
   // todo.completed ? (checkedStyle.textDecoration = "line-through") : null;
   const cardStyles = {
     marginTop: matches ? 20 : 35,
-    background: "lightgray",
+    background: 'lightgray',
   };
   return (
     <Container>
       <Card className="root" variant="outlined" style={cardStyles}>
         <CardContent>
-          <Typography
-            variant="h5"
-            component="h2"
-            style={checkedStyle}
-            className="todo-text"
-          >
-            <IconButton
-              style={{ marginRight: 5 }}
-              onClick={() => markComplete(todo.id)}
-              centerRipple={false}
-            >
+          <Typography variant="h5" component="h2" style={checkedStyle} className="todo-text">
+            <IconButton style={{ marginRight: 5 }} onClick={() => markComplete(todo.id)} centerRipple={false}>
               <CheckTwoToneIcon color="action" />
             </IconButton>
             {todo.title}
-            <IconButton
-              style={{ float: "right" }}
-              onClick={() => {
-                const confirmation = window.confirm("Delete this to-do?");
-                if (confirmation) delTodo(todo.id);
-              }}
-              centerRipple={false}
-            >
+            <IconButton style={{ float: 'right' }} onClick={() => setDeleteOpen(true)} centerRipple={false}>
               <DeleteTwoToneIcon color="error" />
             </IconButton>
-            <IconButton
-              style={{ float: "right" }}
-              onClick={editTodo.bind(this, todo.id, todo.title)}
-              centerRipple={false}
-            >
+            <IconButton style={{ float: 'right' }} onClick={() => setEditOpen(true)} centerRipple={false}>
               <EditTwoToneIcon color="primary" />
             </IconButton>
           </Typography>
         </CardContent>
       </Card>
+      <DeleteConfirm
+        yes={() => {
+          setDeleteOpen(false);
+          setTimeout(() => delTodo(todo.id), 300);
+        }}
+        open={deleteOpen}
+        close={() => setDeleteOpen(false)}
+      />
+      <EditConfirm
+        yes={(val) => {
+          setEditOpen(false);
+          editTodo(todo.id, val);
+        }}
+        open={editOpen}
+        close={() => setEditOpen(false)}
+        value={todo.title}
+      />
     </Container>
   );
 };
