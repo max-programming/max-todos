8c4ddbb7a9816f2bdf8229150ff1c67a9b93f95a
max-programming <maxprogramming05@gmail.com>
2020-10-18 14:28:16 +0530

    Code refractor

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,4 +1,6 @@
-<<<<<<< HEAD
+import { useState } from "react";
+import DeleteConfirm from "./DeleteConfirm";
+import EditConfirm from "./EditConfirm";
 import {
   Card,
   CardContent,
@@ -10,41 +12,49 @@ import {
 import CheckTwoToneIcon from "@material-ui/icons/CheckTwoTone";
 import DeleteTwoToneIcon from "@material-ui/icons/DeleteTwoTone";
 import EditTwoToneIcon from "@material-ui/icons/EditTwoTone";
-=======
-import React, { useState } from 'react';
-import { Card, CardContent, Typography, Container, IconButton, useMediaQuery } from '@material-ui/core';
-import CheckTwoToneIcon from '@material-ui/icons/CheckTwoTone';
-import DeleteTwoToneIcon from '@material-ui/icons/DeleteTwoTone';
-import EditTwoToneIcon from '@material-ui/icons/EditTwoTone';
-import DeleteConfirm from './DeleteConfirm';
-import EditConfirm from './EditConfirm';
->>>>>>> abca0acb0f60b7b268e2f08fbd35c46f6d473fe6
 
 const Todo = ({ todo, markComplete, delTodo, editTodo }) => {
-  const matches = useMediaQuery('(max-width: 768px)');
+  const matches = useMediaQuery("(max-width: 768px)");
   const [deleteOpen, setDeleteOpen] = useState(false);
   const [editOpen, setEditOpen] = useState(false);
-  let checkedStyle = { textDecoration: 'none' };
-  if (todo.completed) checkedStyle.textDecoration = 'line-through';
-  else checkedStyle.textDecoration = 'none';
+  let checkedStyle = { textDecoration: "none" };
+  if (todo.completed) checkedStyle.textDecoration = "line-through";
+  else checkedStyle.textDecoration = "none";
   // todo.completed ? (checkedStyle.textDecoration = "line-through") : null;
   const cardStyles = {
     marginTop: matches ? 20 : 35,
-    background: 'lightgray',
+    background: "lightgray",
   };
   return (
     <Container>
       <Card className="root" variant="outlined" style={cardStyles}>
         <CardContent>
-          <Typography variant="h5" component="h2" style={checkedStyle} className="todo-text">
-            <IconButton style={{ marginRight: 5 }} onClick={() => markComplete(todo.id)} centerRipple={false}>
+          <Typography
+            variant="h5"
+            component="h2"
+            style={checkedStyle}
+            className="todo-text"
+          >
+            <IconButton
+              style={{ marginRight: 5 }}
+              onClick={() => markComplete(todo.id)}
+              centerRipple={false}
+            >
               <CheckTwoToneIcon color="action" />
             </IconButton>
             {todo.title}
-            <IconButton style={{ float: 'right' }} onClick={() => setDeleteOpen(true)} centerRipple={false}>
+            <IconButton
+              style={{ float: "right" }}
+              onClick={() => setDeleteOpen(true)}
+              centerRipple={false}
+            >
               <DeleteTwoToneIcon color="error" />
             </IconButton>
-            <IconButton style={{ float: 'right' }} onClick={() => setEditOpen(true)} centerRipple={false}>
+            <IconButton
+              style={{ float: "right" }}
+              onClick={() => setEditOpen(true)}
+              centerRipple={false}
+            >
               <EditTwoToneIcon color="primary" />
             </IconButton>
           </Typography>
