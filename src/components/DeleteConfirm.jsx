b404b747266c2c6e6048b4a4796b09b24de29a99
Caleb Miller <caleb@mathmammoth.com>
2020-10-22 13:24:48 -0600

    fix it lol

--- src/components/DeleteConfirm.jsx
+++ src/components/DeleteConfirm.jsx
@@ -5,16 +5,14 @@ import {
   DialogContentText,
   DialogTitle,
   Button,
-} from "@material-ui/core";
+} from '@material-ui/core';
 
 const DeleteConfirm = ({ open, close, yes }) => {
   return (
     <Dialog open={open} onClose={close}>
       <DialogTitle>Delete this item?</DialogTitle>
       <DialogContent>
-        <DialogContentText>
-          Are you really sure you want to delete this item?
-        </DialogContentText>
+        <DialogContentText>Are you really sure you want to delete this item?</DialogContentText>
       </DialogContent>
       <DialogActions>
         <Button onClick={close} color="primary">
