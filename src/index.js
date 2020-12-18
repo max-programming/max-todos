c2ff7d45464e1573bccba48012d4a8aa3e147313
max-programming <maxprogramming05@gmail.com>
2020-12-18 12:58:44 +0530

    ✨ Added serviceWorker

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { DeleteConfirmProvider } from "./context/DeleteConfirmContext";
 import { SmallTextProvider } from "./context/SmallTextContext";
 import App from "./App";
 import "./styles.css";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 render(
   <MainProvider>
@@ -18,3 +19,8 @@ render(
   </MainProvider>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
